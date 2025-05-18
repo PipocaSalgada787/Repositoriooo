9786dd81a803ba7b31da9734c8ad884f73558fd7
PipocaSalgada787 <canalscripter201@gmail.com>
2025-05-18 15:38:40 -0300

    Slanej

---
+++ Sla.lua
@@ -0,0 +1,5 @@
+local IceHub = loadstring(game:HttpGet("https://gist.githubusercontent.com/Imperador950/8bdbe1c8003c3645d31ecc08dd6f4954/raw/d7ef405e38759ce397cf5be65babe2a2bc62c92a/Sla"))()
+local MainUI = IceHub.CreateMain()
+= Tab_Scripts: NewSection("Scripts")
+
+House:NewLabel("House Bio")
